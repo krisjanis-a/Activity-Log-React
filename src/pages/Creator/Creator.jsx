e9d936cde333597fe5c1583659bd58329979aa86
krisjanis-a <krishjanis96@gmail.com>
2021-09-21 03:14:51 +0300

    create activity and create group done

--- src/pages/Creator/Creator.jsx
+++ src/pages/Creator/Creator.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Button from "react-bootstrap/Button";
 import "./Creator.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Dropdown from "react-bootstrap/Dropdown";
+import { Context } from "../../context/Context";
+import axios from "axios";
+import Modal from "react-bootstrap/Modal";
 
 const Creator = () => {
   const pageTitle = "Creator";
+  const { user } = useContext(Context);
   const [startDate, setStartDate] = useState(new Date());
   const [activityDurationAutomatic, setActivityDurationAutomatic] = useState(0);
   const [activityDurationString, setActivityDurationString] =
@@ -17,9 +21,22 @@ const Creator = () => {
   const [activityName, setActivityName] = useState("");
   const [activityDurationManual, setActivityDurationManual] = useState("");
   const [useManualDuration, setUseManualDuration] = useState();
-  const [date, setDate] = useState();
   const [commentary, setCommentary] = useState("");
   const [group, setGroup] = useState("");
+  const [errorMessage, setErrorMessage] = useState();
+
+  const [savedGroups, setSavedGroups] = useState([]);
+
+  // Fetch groups
+
+  useEffect(() => {
+    const getGroups = async () => {
+      const res = await axios.get("/groups");
+      const userGroups = res.data.filter((item) => item.userId === user._id);
+      setSavedGroups(userGroups);
+    };
+    getGroups();
+  }, [group, user._id]);
 
   // Timer logic
 
@@ -68,7 +85,76 @@ const Creator = () => {
     return timeStr;
   };
 
+  // Create group modal window
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  // Save new group
+
+  const saveGroup = async () => {
+    const existingGroups = savedGroups.map((group) => group.name);
+    const newGroupName = document.querySelector("#new_group").value;
+
+    if (!existingGroups.includes(newGroupName)) {
+      try {
+        let newGroup = { name: newGroupName, userId: user._id };
+        await axios.post("/Groups", newGroup);
+      } catch (err) {}
+    } else {
+      setErrorMessage("Group already exists");
+    }
+    handleClose();
+    setGroup(newGroupName);
+  };
+
   // Save activity
+
+  const saveActivity = async () => {
+    setErrorMessage("");
+
+    let duration;
+
+    useManualDuration
+      ? (duration = activityDurationManual)
+      : (duration = formatTime(activityDurationAutomatic));
+
+    if (
+      startDate !== null &&
+      activityName !== "" &&
+      /[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/.test(duration)
+    ) {
+      const activity = {
+        username: user.username,
+        userId: user._id,
+        name: activityName,
+        date: startDate.toDateString(),
+        duration: duration,
+        commentary: commentary,
+        group: group,
+      };
+      console.log(activity);
+
+      try {
+        const res = await axios.post("/activities", activity);
+        window.location.replace("/");
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      if (startDate === null) {
+        setErrorMessage("Please enter date");
+      }
+      if (activityName === "") {
+        setErrorMessage("Please enter activity name");
+      }
+      if (/[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/.test(duration) === false) {
+        setErrorMessage("Manual duration must be in format HH:MM:SS");
+      }
+    }
+  };
 
   return (
     <div>
@@ -196,30 +282,65 @@ const Creator = () => {
         <div className="choose-group_container">
           <span className="choose-group_label">Choose group:</span>
           <Dropdown className="filter_group_dropdown w-100">
-            <span className="chosen_group">none</span>
+            <span className="chosen_group">{group ? group : "none"}</span>
             <Dropdown.Toggle
               variant="primary"
               id="dropdown-basic"
             ></Dropdown.Toggle>
 
             <Dropdown.Menu align="end">
-              <Dropdown.Item>none</Dropdown.Item>
-              <Dropdown.Item>Group 1</Dropdown.Item>
-              <Dropdown.Item>Group 2</Dropdown.Item>
-              <Dropdown.Item>Group 3</Dropdown.Item>
+              <Dropdown.Item onClick={() => setGroup("")}>none</Dropdown.Item>
+              {savedGroups.map((item) => (
+                <Dropdown.Item
+                  key={item.name}
+                  onClick={() => setGroup(item.name)}
+                >
+                  {item.name}
+                </Dropdown.Item>
+              ))}
             </Dropdown.Menu>
           </Dropdown>
         </div>
       </div>
       <div className="buttons_container">
         <div className="create_group my-3">
-          <Button variant="primary" className="me-2">
+          <Button variant="primary" className="me-2" onClick={handleShow}>
             Create group
           </Button>
+          <Modal
+            show={showModal}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title className="text-body">Enter Group Name</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <input
+                type="text"
+                id="new_group"
+                placeholder="Group Name"
+                className="w-100"
+                maxLength="20"
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={saveGroup}>
+                Save
+              </Button>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
-        <Button variant="success" className="w-50">
+        <Button variant="success" className="w-50" onClick={saveActivity}>
           Save activity
         </Button>
+        <span className="mt-2 " style={{ color: "red" }}>
+          {errorMessage}
+        </span>
       </div>
     </div>
   );
--- src/pages/Groups/Groups.jsx
+++ src/pages/Groups/Groups.jsx
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Button from "react-bootstrap/Button";
 import Dropdown from "react-bootstrap/Dropdown";
+import { Context } from "../../context/Context";
+import axios from "axios";
+import Modal from "react-bootstrap/Modal";
 
 const Groups = () => {
   const pageTitle = "Groups";
+
+  const { user } = useContext(Context);
+  const [group, setGroup] = useState("");
+  const [savedGroups, setSavedGroups] = useState([]);
+  const [errorMessage, setErrorMessage] = useState();
+
+  useEffect(() => {
+    setErrorMessage();
+  }, [group]);
+
+  // Fetch groups
+
+  useEffect(() => {
+    const getGroups = async () => {
+      const res = await axios.get("/groups");
+      const userGroups = res.data.filter((item) => item.userId === user._id);
+      setSavedGroups(userGroups);
+    };
+    getGroups();
+  }, [group, user._id]);
+
+  // Create group modal window
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  const [showModalDel, setShowModalDel] = useState(false);
+
+  const handleCloseDel = () => setShowModalDel(false);
+  const handleShowDel = () => setShowModalDel(true);
+
+  // Save new group
+
+  const saveGroup = async () => {
+    const existingGroups = savedGroups.map((group) => group.name);
+    const newGroupName = document.querySelector("#new_group").value;
+
+    if (!existingGroups.includes(newGroupName)) {
+      try {
+        let newGroup = { name: newGroupName, userId: user._id };
+        await axios.post("/Groups", newGroup);
+      } catch (err) {}
+    } else {
+      setErrorMessage("Group already exists");
+    }
+    handleClose();
+    setGroup(newGroupName);
+  };
+
+  // Delete group
+
+  const deleteGroup = async () => {
+    console.log("deleting group");
+  };
 
   return (
     <div>
@@ -12,31 +71,108 @@ const Groups = () => {
       <div className="choose-group_container">
         <span className="choose-group_label">Choose group:</span>
         <Dropdown className="filter_group_dropdown w-100">
-          <span className="chosen_group">none</span>
+          <span className="chosen_group">{group ? group : "none"}</span>
           <Dropdown.Toggle
             variant="primary"
             id="dropdown-basic"
           ></Dropdown.Toggle>
 
           <Dropdown.Menu align="end">
-            <Dropdown.Item>none</Dropdown.Item>
-            <Dropdown.Item>Group 1</Dropdown.Item>
-            <Dropdown.Item>Group 2</Dropdown.Item>
-            <Dropdown.Item>Group 3</Dropdown.Item>
+            <Dropdown.Item onClick={() => setGroup("")}>none</Dropdown.Item>
+            {savedGroups.map((item) => (
+              <Dropdown.Item
+                key={item.name}
+                onClick={() => setGroup(item.name)}
+              >
+                {item.name}
+              </Dropdown.Item>
+            ))}
           </Dropdown.Menu>
         </Dropdown>
       </div>
 
       <div className="buttons_container">
         <div className="create_delete_group my-3">
-          <Button variant="primary" className="me-2">
+          <Button variant="primary" className="me-2" onClick={handleShow}>
             Create group
           </Button>
-          <Button variant="danger" className="ms-2">
+          <Modal
+            show={showModal}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title className="text-body">Enter Group Name</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <input
+                type="text"
+                id="new_group"
+                placeholder="Group Name"
+                className="w-100"
+                maxLength="20"
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={saveGroup}>
+                Save
+              </Button>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+          <Button
+            variant="danger"
+            className="ms-2"
+            disabled={group === ""}
+            onClick={handleShowDel}
+          >
             Delete group
           </Button>
+          <Modal
+            show={showModalDel}
+            // onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header style={{ display: "flex", justifyContent: "center" }}>
+              <Modal.Title className="text-body">
+                Confirm delete group
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body
+              className="w-100"
+              style={{ display: "flex", justifyContent: "center" }}
+            >
+              <Button
+                variant="danger"
+                className="me-2"
+                onClick={() => (deleteGroup(), handleCloseDel())}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="secondary"
+                className="ms-2"
+                onClick={handleCloseDel}
+              >
+                Cancel
+              </Button>
+            </Modal.Body>
+          </Modal>
         </div>
       </div>
+      <p
+        className="mt-2 w-100"
+        style={{
+          color: "red",
+          textAlign: "center",
+        }}
+      >
+        {errorMessage}
+      </p>
     </div>
   );
 };
