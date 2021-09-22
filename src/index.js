87c6acf6e28ec2047c06d5d4fe7b124a212025a6
krisjanis-a <krishjanis96@gmail.com>
2021-09-23 02:17:09 +0300

    first build version

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ContextProvider } from "./context/Context";
-// import axios from "axios";
+import axios from "axios";
 
-// axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
-// if (process.env.NODE_ENV === "production") {
-// axios.defaults.baseURL = "https://react-blog-free-world.herokuapp.com/api/";
-// }
+if (process.env.NODE_ENV === "production") {
+  axios.defaults.baseURL = "https://activity-log-react.herokuapp.com/server/";
+}
 
 ReactDOM.render(
   <React.StrictMode>
